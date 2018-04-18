3a13fb503be6c5de1bab251ccb184c194c1af592
AmandaRM <amandaruma@gmail.com>
2018-04-18 14:45:38 -0300

    json linhas comentadas

--- EP.py
+++ EP.py
@@ -8,14 +8,12 @@ Created on Tue Apr 17 18:26:32 2018
 
 #========= fazendo o menu =========
 import json
-escolha = 1
-#estoquestr=
-#if estoquestr[0]=='{':
-#    estoque = json.loads(estoquestr)
-#else:
-#    estoque={}
-estoque={}
-while 5 > escolha > 0:
+with open ('estoque_python.json', 'r') as arquivo:
+    estoque=arquivo.read()    
+if len(estoque)==0:
+    estoque={}
+else:
+    estoque=json.loads(estoque)
     print ("Controle do Estoque:")
     print ("0 - sair ")
     print ("1 - adicionar item")
@@ -23,35 +21,44 @@ while 5 > escolha > 0:
     print ("3 - alterar item")
     print ("4 - imprimir estoque")
     escolha = int(input("Faça sua escolha: "))
-    if escolha == 1:
-        produto = input("Nome do produto: ")
-        quantidade_inicial = int(input("Quantidade inicial: "))
-        estoque[produto]=quantidade_inicial
-        if quantidade_inicial < 0:
-            print ("A quantidade inicial não pode ser negativa.")
+    while 5 > escolha > 0:
+        print ("Controle do Estoque:")
+        print ("0 - sair ")
+        print ("1 - adicionar item")
+        print ("2 - remover item")
+        print ("3 - alterar item")
+        print ("4 - imprimir estoque")
+        escolha = int(input("Faça sua escolha: "))
+        if escolha == 1:
+            produto = input("Nome do produto: ")
             quantidade_inicial = int(input("Quantidade inicial: "))
-    elif escolha ==2:
-        produto = input ("Nome do produto a ser removido: ")
-        if produto not in estoque:
-            print ("Elemento não encontrado")
-        elif produto in estoque:
-            del estoque[produto]
-    elif escolha == 3:
-        produto = input ("Nome do produto que deseja alterar: ")
-        if produto not in estoque:
-            print ("Elemento não encontrado")
-        elif produto in estoque:
-            quantidade = int(input("Quantidade: "))
-            estoque[produto]+=quantidade
-            print ('Novo estoque de {0}: {1}'.format(produto, estoque[produto]))
-            # ver o que tem de errado nessa linha de cima
-    elif escolha == 4:
-        #copia_estoque = estoque.split(',')
-        print (estoque.split(','))
-if escolha == 0:
-    estoquestr=json.dump(estoque)
-    print ("Até mais")
-
+            estoque[produto]=quantidade_inicial
+            if quantidade_inicial < 0:
+                print ("A quantidade inicial não pode ser negativa.")
+                quantidade_inicial = int(input("Quantidade inicial: "))
+        elif escolha ==2:
+            produto = input ("Nome do produto a ser removido: ")
+            if produto not in estoque:
+                print ("Elemento não encontrado")
+            elif produto in estoque:
+                del estoque[produto]
+        elif escolha == 3:
+            produto = input ("Nome do produto que deseja alterar: ")
+            if produto not in estoque:
+                print ("Elemento não encontrado")
+            elif produto in estoque:
+                quantidade = int(input("Quantidade: "))
+                estoque[produto]+=quantidade
+                print ('Novo estoque de {0}: {1}'.format(produto, estoque[produto]))
+                # ver o que tem de errado nessa linha de cima
+        elif escolha == 4:
+            #copia_estoque = estoque.split(',')
+            print (estoque.split(','))
+    if escolha == 0:
+        estoquestr=json.dump(estoque)
+        print ("Até mais")
+with open ('estoque_python.json', 'w') as arquivo:
+    arquivo.write(estoquestr)
     
     
     
